bad5d7c2ceeb9623ad581586fae0340bc1676962
Nilu patel <nilusingh945164@gmail.com>
2024-02-21 09:13:45 +0530

    r

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Learn react js
         </p>
         <a
           className="App-link"
