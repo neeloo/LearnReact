b4ea54945d6a574572d513856289976b1b8c6dc2
Nilu patel <nilusingh945164@gmail.com>
2024-02-22 19:35:57 +0530

    p

---
+++ src/Movie.js
@@ -0,0 +1,12 @@
+import './Movie.css'
+import Header from './Movie Project/Header'
+
+function Movie() {
+  return (
+    <div className='app'>
+        <Header/>
+    </div>
+  )
+}
+
+export default Movie
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+//projects
+import Movie from './Movie';
+
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Movie/>
   </React.StrictMode>
 );
 
