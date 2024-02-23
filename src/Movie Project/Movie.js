7f82940ea261904e8e0d43f5a90bdf994d102f47
Nilu patel <nilusingh945164@gmail.com>
2024-02-23 15:20:28 +0530

    m

--- src/Movie Project/Movie.js
+++ src/Movie Project/Movie.js
@@ -1,10 +1,14 @@
 import './Movie.css'
 import Header from './Header'
+import Movie1 from './Movie1'
 
 function Movie() {
   return (
-    <div className='app'>
+    <div className='header'>
         <Header/>
+        <div className="movie">
+          <Movie1/>
+        </div>
     </div>
   )
 }
---
+++ src/Movie Project/Movie1.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Movie1() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Movie1
