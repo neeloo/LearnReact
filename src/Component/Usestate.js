02064925c9e2ca4b3202090f7176a8519f3eab5b
Nilu patel <nilusingh945164@gmail.com>
2024-02-28 16:26:36 +0530

    d

--- src/Component/Usestate.js
+++ src/Component/Usestate.js
@@ -1,10 +1,36 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Header from '../Movie Project/Header'
 
 function Usestate() {
+  const[num,setnum]=useState(1);
+
+  function inc(){
+    setnum(num+1);
+  }
+  
+  function dec(){
+    if(num>0){
+      setnum(num-1);
+    }else {
+      setnum (num);
+    }
+    
+  }
+
+
+
+
+
   return (
     <div className='nss'>
     <Header/>
+    <div className='main'>
+      <h1 className='head'>{num}</h1>
+      <div className="buttons">
+      <button className='btn' onClick={inc}>increment</button>
+      <button className='btn' onClick={dec}>decrement</button>
+      </div>
+    </div>
 
     </div>
   )
--- src/Movie Project/Header.js
+++ src/Movie Project/Header.js
@@ -5,7 +5,8 @@ function Header() {
     
     <div className='header'>
 
-        <h1>Movie Review</h1>
+        {/* <h1>Movie Review</h1> */}
+        <h1>useState</h1>
 
     </div>
   )
